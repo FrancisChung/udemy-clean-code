fcb1ad7bc2017a8506feec78df013404b7643bde
FrancisChung <francis.c.chung@gmail.com>
2020-03-06 06:32:56 +1100

    Page_Load refactored to TrySavePost and DisplayPost. Refactored DBContext variable

--- CleanCode/FullRefactoring/Example1.cs
+++ CleanCode/FullRefactoring/Example1.cs
@@ -9,58 +9,63 @@ namespace Project.UserControls
 {
     public class PostControl : System.Web.UI.UserControl
     {
-        private PostDbContext DBContext;
+        private readonly PostDbContext _dbContext;
 
-
+        public PostControl()
+        {
+            _dbContext = new PostDbContext();
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
-            DBContext = new PostDbContext();
-
             if (Page.IsPostBack)
+                TrySavePost();
+            else
+                DisplayPost();
+        }
+
+        private void DisplayPost()
+        {
+            Post entity = _dbContext.Posts.SingleOrDefault(p => p.Id == Convert.ToInt32(Request.QueryString["id"]));
+            PostBody.Text = entity.Body;
+            PostTitle.Text = entity.Title;
+        }
+
+        private void TrySavePost()
+        {
+            PostValidator validator = new PostValidator();
+            Post entity = new Post()
             {
-                PostValidator validator = new PostValidator();
-                Post entity = new Post()
-                {
-                    // Map form fields to entity properties
-                    Id = Convert.ToInt32(PostId.Value),
-                    Title = PostTitle.Text.Trim(),
-                    Body = PostBody.Text.Trim()
-                };
-                ValidationResult results = validator.Validate(entity);
+                // Map form fields to entity properties
+                Id = Convert.ToInt32(PostId.Value),
+                Title = PostTitle.Text.Trim(),
+                Body = PostBody.Text.Trim()
+            };
+            ValidationResult results = validator.Validate(entity);
 
-                if (results.IsValid)
-                {
-                    // Save to the database and continue to the next page
-                    DBContext.Posts.Add(entity);
-                    DBContext.SaveChanges();
-                }
-                else
-                {
-                    BulletedList summary = (BulletedList)FindControl("ErrorSummary");
-
-                    // Display errors to the user
-                    foreach (var failure in results.Errors)
-                    {
-                        Label errorMessage = FindControl(failure.PropertyName + "Error") as Label;
-
-                        if (errorMessage == null)
-                        {
-                            summary.Items.Add(new ListItem(failure.ErrorMessage));
-                        }
-                        else
-                        {
-                            errorMessage.Text = failure.ErrorMessage;
-                        }
-                    }
-                }
+            if (results.IsValid)
+            {
+                // Save to the database and continue to the next page
+                _dbContext.Posts.Add(entity);
+                _dbContext.SaveChanges();
             }
             else
             {
-                // Display form
-                Post entity = DBContext.Posts.SingleOrDefault(p => p.Id == Convert.ToInt32(Request.QueryString["id"]));
-                PostBody.Text = entity.Body;
-                PostTitle.Text = entity.Title;
+                BulletedList summary = (BulletedList) FindControl("ErrorSummary");
 
+                // Display errors to the user
+                foreach (var failure in results.Errors)
+                {
+                    Label errorMessage = FindControl(failure.PropertyName + "Error") as Label;
+
+                    if (errorMessage == null)
+                    {
+                        summary.Items.Add(new ListItem(failure.ErrorMessage));
+                    }
+                    else
+                    {
+                        errorMessage.Text = failure.ErrorMessage;
+                    }
+                }
             }
         }
 
